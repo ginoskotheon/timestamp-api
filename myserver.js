d820363be0069ff1f6020b9fb391d62829f5c9de
William <ginoskotheon@yahoo.com>
2016-09-26 19:18:33 +0000

    new files and corrections

---
+++ myserver.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+
+var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+
+
+//get index.html file
+
+app.get('/', function(req, res){
+    res.sendFile(path.join(__dirname+'/index.html'));
+});
+
+//get the date and set format
+
+app.get('/:date', function(req, res){
+    var param = req.params.date;
+    param = /^\d+$/.test(param) ? parseInt(param) * 1000 : param;
+    
+    var date = new Date(param);
+    var result = {
+        unix: null,
+        natural: null
+    };
+    if(!isNaN(date)){
+        result.unix = Math.floor(date.getTime() / 1000 | 0);
+        result.natural = months[date.getMonth()] + ' ' + date.getDate() + ', ' + date.getFullYear();
+    }
+    res.json(result);
+});
+
+app.listen(8080, function(){
+    console.log('Example app listening on port 8080!');
+});
